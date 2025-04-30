d811b09d8ccf94ebd13e588027f30073b5873779
popcorn <popcorn088@gmail.com>
2025-04-30 15:56:50 +0900

    NumericUpDown追加

---
+++ UserControlsWPF/NumericUpDown/NumericUpDown.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControlsWPF.NumericUpDown
+{
+    /// <summary>
+    /// NumericUpDown.xaml の相互作用ロジック
+    /// </summary>
+    public partial class NumericUpDown : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty
+            = DependencyProperty.Register(
+                nameof(Value),
+                typeof(decimal),
+                typeof(NumericUpDown),
+                new PropertyMetadata(0m));
+        public decimal Value
+        {
+            get => (decimal)this.GetValue(ValueProperty);
+            set => this.SetValue(ValueProperty, value);
+        }
+        public static readonly DependencyProperty NickProperty
+            = DependencyProperty.Register(
+                nameof(Nick),
+                typeof(decimal),
+                typeof(NumericUpDown),
+                new PropertyMetadata(1m));
+        public decimal Nick
+        {
+            get => (decimal)this.GetValue(NickProperty);
+            set => this.SetValue(NickProperty, value);
+        }
+        public static readonly DependencyProperty UpCommandProperty
+            = DependencyProperty.Register(
+                nameof(UpCommand),
+                typeof(ICommand),
+                typeof(NumericUpDown));
+        public ICommand UpCommand
+        {
+            get => (ICommand)this.GetValue(UpCommandProperty);
+            set => this.SetValue(UpCommandProperty, value);
+        }
+        public static readonly DependencyProperty DownCommandProperty
+            = DependencyProperty.Register(
+                nameof(DownCommand),
+                typeof(ICommand),
+                typeof(NumericUpDown));
+        public ICommand DownCommand
+        {
+            get => (ICommand)this.GetValue(DownCommandProperty);
+            set => this.SetValue(DownCommandProperty, value);
+        }
+        public static readonly DependencyProperty MaximumProperty
+            = DependencyProperty.Register(
+                nameof(Maximum),
+                typeof(decimal),
+                typeof(NumericUpDown),
+                new PropertyMetadata(100m));
+        public decimal Maximum
+        {
+            get => (decimal)this.GetValue(MaximumProperty);
+            set => this.SetValue(MaximumProperty, value);
+        }
+        public static readonly DependencyProperty MinimumProperty
+            = DependencyProperty.Register(
+                nameof(Minimum),
+                typeof(decimal),
+                typeof(NumericUpDown),
+                new PropertyMetadata(0m));
+        public decimal Minimum
+        {
+            get => (decimal)this.GetValue(MinimumProperty);
+            set => this.SetValue(MinimumProperty, value);
+        }
+        public static readonly DependencyProperty StringFormatProperty
+            = DependencyProperty.Register(
+                nameof(StringFormat),
+                typeof(string),
+                typeof(NumericUpDown),
+                new PropertyMetadata(string.Empty));
+        public string StringFormat
+        {
+            get => (string)this.GetValue(StringFormatProperty);
+            set => this.SetValue(StringFormatProperty, value);
+        }
+        public NumericUpDown()
+        {
+            InitializeComponent();
+            UpCommand = new RelayCommand(UpCommandExecute);
+            DownCommand = new RelayCommand(DownCommandExecute);
+        }
+
+        private void DownCommandExecute()
+        {
+            if (Value - Nick >= Minimum)
+            {
+                Value -= Nick;
+            }
+        }
+
+        private void UpCommandExecute()
+        {
+            if (Value + Nick <= Maximum)
+            {
+                Value += Nick;
+            }
+        }
+    }
+
+    public class DecimalConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is decimal dec)
+            {
+                return dec.ToString((string)values[1]);
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ UserControlsWPF/RelayCommand.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UserControlsWPF
+{
+    // https://sourcechord.hatenablog.com/entry/2014/01/13/200039
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute) : this(execute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+        public event EventHandler? CanExecuteChanged;
+        public RelayCommand(Action<T> execute)
+        {
+            _execute = execute;
+        }
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+}
